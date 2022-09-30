57bf35c7b85dcf4eab945fda4b9670346bd0bc55
37stu37 <37stu37@gmail.com>
2022-09-30 12:43:49 +0100

    c

--- prepro_bldg_data.py
+++ prepro_bldg_data.py
@@ -1,10 +1,11 @@
 import geopandas as gpd
 from pathlib import Path
 
-server = Path("Y://_TEMP//Server_Gitdata")
+# server = Path("Y://_TEMP//Server_Gitdata")
+server = Path("/Volumes/Nepal/SajagNepal/003_GIS_data/_TEMP/Server_Gitdata")
 
-b = gpd.read_file(server / "Bhojpur_Mw5.1\shapefile\bldgs_preprocs_E4.shp")
-d = gpd.read_file(server / "Bhojpur_Mw5.1\shapefile\Nepal_Districts_v2020_UTM45shp.shp")
+b = gpd.read_file(server / "Bhojpur_Mw5.1" / "shapefile" / "bldgs_preprocs_E4.shp")
+d = gpd.read_file(server / "Bhojpur_Mw5.1" / "shapefile" / "Nepal_Districts_v2020_UTM45shp.shp")
 
 # create centroid to prevent overlap of polygons
 c = b.centroids
@@ -13,4 +14,4 @@ c = b.centroids
 c_d = c.sjoin(d)
 
 # export shapefile
-c_d = gpd.to_file(server / "Bhojpur_Mw5.1\shapefile\bldgs_preprocs_E4_DISTRICTS.shp")
+c_d = gpd.to_file(server / "Bhojpur_Mw5.1" / "shapefile" / "bldgs_preprocs_E4_DISTRICTS.shp")
