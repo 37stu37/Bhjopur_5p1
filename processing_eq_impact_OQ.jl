39da085d3467785cc348d45aa635fa6bfa2ab32a
37stu37 <37stu37@gmail.com>
2022-10-04 14:32:37 +0100

    working!

--- processing_eq_impact_OQ.jl
+++ processing_eq_impact_OQ.jl
@@ -25,22 +25,14 @@ end
 
 
 # path
-server = "Y://_TEMP//Server_Gitdata"
-# server = "/Volumes/Nepal/SajagNepal/003_GIS_data/_TEMP/Server_Gitdata"
+# server = "Y://_TEMP//Server_Gitdata"
+server = "/Volumes/Nepal/SajagNepal/003_GIS_data/_TEMP/Server_Gitdata"
 directory = joinpath(server, "Bhojpur_Mw5.1")
 
 
 # data
-gmf = CSV.read(joinpath(directory, "OQoutputs", "output-7-gmf_data-csv",  "gmf-data_12.csv"), header=3, DataFrame)
-sitemesh = CSV.read(joinpath(directory, "OQoutputs", "output-7-gmf_data-csv",  "sitemesh_12.csv"), DataFrame)
-
-# merge OQ output together
-OQ = leftjoin(sitemesh, gmf, on= :site_id)
-OQ = coalesce.(OQ, 0.0)
-
-# merge OQ outputs with the building dataset to do analysis
-b = DataFrame(Shapefile.Table(joinpath(directory, "shapefile", "bldgs_preprocs_E4.shp")))
-b = leftjoin(b, OQ, on = :su_id => :site_id)
+b = CSV.read(joinpath(directory, "OQoutputs/20", "bldg_NN_OQrun_20_reprojectedUTM45.csv"), DataFrame)
+# b= CSV.read("/Volumes/Nepal/SajagNepal/003_GIS_data/_TEMP/Server_Gitdata/Bhojpur_Mw5.1/OQoutputs/20/bldg_NN_OQrun_20_reprojectedUTM45.csv", DataFrame)
 
 ################################## EARTHQUAKE IMPACT #############################
 
@@ -48,33 +40,34 @@ b = leftjoin(b, OQ, on = :su_id => :site_id)
 typo = @. JSON.parse(b.constructi)
 
 
-# fragilities paramters per typologies1.90, 0.93
 
-# Hazus(http://drm.cenn.org/Trainings/Multi%20Hazard%20Risk%20Assessment/Lectures_ENG/Session%2006%20Risk%20Analysis/Background/HAZUS%20EQ_TM_Chapter05%20building%20losses.pdf)
-C99ref = ["Didier et al., 2017", "Gautam et al., 2018", "Hazus C3M low code"]
-C99mean = [1.95, 1.29, 0.51]
-C99scale = [0.71, 0.83, 0.64]
+###### fragilities paramters per typologies1.90, 0.93
+begin
+    # Hazus(http://drm.cenn.org/Trainings/Multi%20Hazard%20Risk%20Assessment/Lectures_ENG/Session%2006%20Risk%20Analysis/Background/HAZUS%20EQ_TM_Chapter05%20building%20losses.pdf)
+    C99ref = ["Didier et al., 2017", "Gautam et al., 2018", "Hazus C3M low code"]
+    C99mean = [1.95, 1.29, 0.51]
+    C99scale = [0.71, 0.83, 0.64]
 
-# MURref = ["Guragain et al., 2015", "Gautam et al., 2018", "Didier et al., 2017"]
-# MURmean = [0.23, 0.22, 1.26]
-# MURscale = [0.31, 0.48, 1.96]
+    # MURref = ["Guragain et al., 2015", "Gautam et al., 2018", "Didier et al., 2017"]
+    # MURmean = [0.23, 0.22, 1.26]
+    # MURscale = [0.31, 0.48, 1.96]
 
-MURref = ["Didier et al., 2017 - Brick Cement", "Guragain et al., 2015", "Didier et al., 2017 - Brick Mud"]
-MURmean = [1.9, 0.23, 1.26]
-MURscale = [0.93, 0.31, 1.96]
+    MURref = ["Didier et al., 2017 - Brick Cement", "Guragain et al., 2015", "Didier et al., 2017 - Brick Mud"]
+    MURmean = [1.9, 0.23, 1.26]
+    MURscale = [0.93, 0.31, 1.96]
 
-MUR_STRUBref = ["Gautam et al., 2018", "Guragain et al., 2015", "Didier et al., 2017"]
-MUR_STRUBmean = [0.39, 0.203, 1.26]
-MUR_STRUBscale = [0.69, 0.308, 1.96]
+    MUR_STRUBref = ["Gautam et al., 2018", "Guragain et al., 2015", "Didier et al., 2017"]
+    MUR_STRUBmean = [0.39, 0.203, 1.26]
+    MUR_STRUBscale = [0.69, 0.308, 1.96]
 
-Sref = ["Hazus S3 moderate code", "Hazus S3 low code", "Hazus S3 low code"]
-Smean = [0.60, 0.38, 0.38]
-Sscale = [0.64, 0.64, 0.64]
+    Sref = ["Hazus S3 moderate code", "Hazus S3 low code", "Hazus S3 low code"]
+    Smean = [0.60, 0.38, 0.38]
+    Sscale = [0.64, 0.64, 0.64]
 
-Wref = ["Hazus W1 moderate code", "Hazus W1 low code", "Hazus W1 low code"]
-Wmean = [1.34, 0.95, 0.95]
-Wscale = [0.64, 0.64, 0.64]
-
+    Wref = ["Hazus W1 moderate code", "Hazus W1 low code", "Hazus W1 low code"]
+    Wmean = [1.34, 0.95, 0.95]
+    Wscale = [0.64, 0.64, 0.64]
+end
 
 # iterate METEOR dictionay at each building to get fragility for pga values
 list_FRAGILITY = []
@@ -136,10 +129,33 @@ b[!, :mid_case] = r[2,:]
 b[!, :high_case] = r[3,:]
 
 # get number of impact per districts
-r_count = combine(groupby(r, :DISTRICT), [:low_case, :mid_case, :high_case] .=> sum)
+r_count = combine(groupby(b, :DISTRICT), [:low_case, :mid_case, :high_case] .=> sum)
+filter!(:DISTRICT => x -> !(ismissing(x)), r_count)
+sort!(r_count, [:DISTRICT], rev=true)
 
-# Plot
+# bar plot
 bar(1:length(r_count.DISTRICT), Matrix(r_count[!, [:high_case_sum, :mid_case_sum, :low_case_sum]]), width=0.9,
         fill=["lightred", "lightgreen", "lightblue"],
         yticks=Tuple([x for x in r_count.DISTRICT]),
-        hbar=true)
+        xlabel="number of buildings with potential severe damage",
+        # ylabel="Districts",
+        figsize=(20, 50),
+        hbar=true,
+        savefig=joinpath(pwd(), "OQ_run20_barplot_DISTRICTS.png"),
+        show=true)
+
+# Map
+shp = gmtread(joinpath(directory, "OQoutputs/20", "r_countDISTRICTS_OQrun_20.shp"))
+hypocenter = gmtread(joinpath(directory, "OQoutputs/"))
+c = r_count[:, 2]
+D = r_count[:, 1]
+D = convert(Array{String}, D)
+
+zvals = polygonlevels(shp, D, c, att="DISTRICT", nocase=true);
+C = makecpt(range=(0,300,10), inverse=true, cmap=:hot, alpha=50);
+
+# grdimage("@earth_relief_30s", region=(79,89,26,30.7), proj=:guess, shade=true) #figsize=10, 
+plot(shp, level=zvals, cmap=C, A=50, pen=0.5, region=(79,89,26,30.7), lw=0.1, proj=:guess)
+plot!()
+colorbar!(pos=(anchor=:MR,length=(7,0.4), offset=(.5,-0)), color=C,
+          axes=(annot=50,), show=true)
